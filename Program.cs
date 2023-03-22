f71e6d1d7d5cb8ce93ebad64a5aa09fda451e941
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-03-21 22:30:56 -0400

    + fixed print method

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Game.Move(7, 4, 3);
-        Bitboard.Debug(Game.BitBoards[3]);
+        Bitboard.Debug(Game.BitBoards[10]);
     }
 }
--- WOCChess/WOCChess.Game/Bitboard.cs
+++ WOCChess/WOCChess.Game/Bitboard.cs
@@ -1,10 +1,12 @@
+using System.Numerics;
+
 namespace WOCChess.Game
 {
     public static class Bitboard
     {
         public static void Debug(ulong board)
         {
-            IEnumerable<string> ranks = Enumerable.Range(0, 8).Select(i => Convert.ToString((long)board, 2).Substring(i * 8, 8)).Reverse();
+            IEnumerable<string> ranks = Enumerable.Range(0, 8).Select(i => $"{String.Concat(Enumerable.Repeat('0', BitOperations.LeadingZeroCount(board)))}{Convert.ToString((long)board, 2)}".Substring(i * 8, 8));
             foreach (string rank in ranks)
             {
                 Console.WriteLine($"{rank[0]}  {rank[1]}  {rank[2]}  {rank[3]}  {rank[4]}  {rank[5]}  {rank[6]}  {rank[7]}");
